28cd2df92ffed5997e4663e12fb3654ab74d307c
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-29 04:56:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,7 @@ def using_pop(array)
   array.pop
 end
 
-def using_pop_with_args(array)
+def pop_with_args(array)
   array.push[2]
 end
 
@@ -21,7 +21,7 @@ def using_shift(array)
   array.shift
 end
 
-def using_shift_with_args(array)
+def shift_with_args(array)
   array.shift[2]
 end
 
